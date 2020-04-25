8be0822a7f3ddf01b9e71a7c1568a03d4729491f
Rushad Mehta <rushadmehta16@gmail.com>
2020-04-26 00:21:01 +0530

    Improved data logging functionality. Removed redundant data logging code. The get_my_ip() method was removed and direct log writes were called from each of the other required methods. Added information to be displayed at every action to the command line for ease of troubleshooting in the future.

--- server.py
+++ server.py
@@ -8,13 +8,6 @@ import os
 camera = cv2.VideoCapture(0)
 app = Flask(__name__)
 
-def get_my_ip():
-    log = open("user_logs.log", "a")
-    log_data = str(datetime.datetime.now()) + "," +  str(username) + "," + str(request.remote_addr) + "\n"
-    if login_status:
-        log.write(log_data)
-        log.close()
-
 def gen_frames():
     while True:
         success, frame = camera.read()
@@ -31,6 +24,7 @@ def gen_frames():
 @app.route('/home')
 def home():
     """Login page."""
+    print("On Home Page")
     return render_template('home.html')
 
 @app.route('/register',  methods=['POST'])
@@ -54,7 +48,15 @@ def add_user():
             user_details.write(username_sha256 + "," + password_sha256 + "\n")
             user_details.close()
         else:
+            print("Registration Failed: Username already exists")
+            data_log = open("data_log.log", "a")
+            data_log.write( "None" + "," + str(request.remote_addr) + ",Registration Unsuccessful," + str(datetime.datetime.now()) + "\n")
+            data_log.close()
             return "<h1>Username already taken. Please try again with a different username</h1>"
+        data_log = open("data_log.log", "a")
+        data_log.write( str(username) + "," + str(request.remote_addr) + ",Registration Successful," + str(datetime.datetime.now()) + "\n")
+        data_log.close()
+        print("Registration Successful: " + str(username))
         return render_template('reg_conf.html')
     
     else:
@@ -98,7 +100,10 @@ def dashboard():
     if username_sha256 in list(user_details["username"]):
         if password_sha256 == password_extract["password"][username_sha256]:
             login_status = True
-            get_my_ip()
+            data_log = open("data_log.log", "a")
+            data_log.write( str(username) + "," + str(request.remote_addr) + ",Logged In," + str(datetime.datetime.now()) + "\n")
+            data_log.close()
+            print("Logged In: " + str(username))
             return render_template('front.html')
         else:
             return "<h1>Wrong Login credentials. Please try again</h1>"
@@ -115,6 +120,10 @@ def video_feed():
 def live_stream():
     path = request.form['vid_stream']
     if path == "OPEN LIVE STREAM":
+        data_log = open("data_log.log", "a")
+        data_log.write( str(username) + "," + str(request.remote_addr) + ",Accessed Live Stream," + str(datetime.datetime.now()) + "\n")
+        data_log.close()
+        print("Live Stream Accessed")
         return render_template('stream_page.html')
 
 @app.route('/file_upload', methods=['POST'])
@@ -129,6 +138,10 @@ def document_network():
     if path == "OPEN DOCUMENT NETWORK":
         docs = os.listdir(r"C:\Users\Rushad\Desktop\Home-Network\file_storage")
         docs_str = "<h2>Files currently hosted :</h2><br>"
+        data_log = open("data_log.log", "a")
+        data_log.write( str(username) + "," + str(request.remote_addr) + ",Viewed Hosted Documents List," + str(datetime.datetime.now()) + "\n")
+        data_log.close()
+        print("Viewed Hosted Documents")
         for i in range(len(docs)):
             docs_str = docs_str + "<h3>" + str(docs[i]) + "</h3>" + "<br>"
         return docs_str
@@ -146,7 +159,16 @@ def search():
     final_path = file_dir + "\\" + str(path)
     if len(path) > 0:
         if os.path.isfile(final_path):
-            print("Accessing : " + str(path))
+            print("File Search Initiated: " + str(path))
+        else :
+            print("File not Found")
+            data_log = open("data_log.log", "a")
+            data_log.write( str(username) + "," + str(request.remote_addr) + ",File not Found: " + str(path) + ","+ str(datetime.datetime.now()) + "\n")
+            data_log.close()
+    data_log = open("data_log.log", "a")
+    data_log.write( str(username) + "," + str(request.remote_addr) + ",Accessed File: " + str(path) + ","+ str(datetime.datetime.now()) + "\n")
+    data_log.close()
+    print("Accessing : " + str(path))
     return send_file(final_path)
         
 
@@ -156,6 +178,10 @@ def handleUpload():
         file = request.files['files']
         if file.filename != '':            
             file.save(os.path.join('C:/Users/Rushad/Desktop/Home-Network/file_storage', file.filename))
+            data_log = open("data_log.log", "a")
+            data_log.write( str(username) + "," + str(request.remote_addr) + ",Uploaded File " + str(file) + "," + str(datetime.datetime.now()) + "\n")
+            data_log.close()
+            print("File Uploaded: " + str(file))
             return render_template('dash_return.html')
         else:
             return "<h1>Upload Failed<h1>"
@@ -165,6 +191,7 @@ def handleUpload():
 def return_home():
     path = request.form['ret_dash']
     if path == "return":
+        print("Returning to Home Screen")
         return render_template('home.html')
 
 
@@ -189,4 +216,4 @@ def server_error(e):
     return render_template("500.html"), 500
 
 if __name__ == '__main__':
-    app.run(host='192.168.43.81')
+    app.run(host='192.168.43.80')
